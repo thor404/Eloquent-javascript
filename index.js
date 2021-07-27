aa5d4bc5469290b7256fd7bd6348b636b9d963d6
ehalt <torikussadikk@gmail.com>
2021-07-27 08:12:55 +0600

    jsbook

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+// ---------- special numbers -------
+
+// there are 3 special values in javaScript that are considered numbers but don't behave like normal numbers.
+
+// infinity
+// -infinity
+// NaN
+
+// -------- String ---------
+
+// ` Down on the sea `
+// "Lie on the ocean"
+// 'Float on the ocean'
+
+// "This is the first line \nAnd this is the second line "
+
+
+
+// String cannot be divided, multiplied, or subtracted, but the + operator can be used on them
+
+// "con" + "cat" + "e" + "nate"
+
+// `half of 100 is ${100 / 2}`
+
+
+
+// ------------ unary operators ------------
+
+// not all operators are symbols some are written as words.
+
+console.log(typeof 3)
